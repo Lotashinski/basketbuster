d9075213fec7b8ce0d1624050fb4624f5eac478e
Alexander Lotashinsky <lat443152@gmail.com>
2024-04-15 23:12:49 +0300

    fix changelog

--- services/user-service/src/main/java/com/github/lotashinski/basketbuster/userservice/entity/UserReview.java
+++ services/user-service/src/main/java/com/github/lotashinski/basketbuster/userservice/entity/UserReview.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
@@ -27,9 +28,11 @@ public class UserReview {
     private int starts;
     
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
     
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
     private Product product;
     
 }
