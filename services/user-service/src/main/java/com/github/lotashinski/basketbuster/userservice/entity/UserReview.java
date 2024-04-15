c25d9032c0238bf2d92e1946f994b380e86958ce
Alexander Lotashinsky <lat443152@gmail.com>
2024-04-15 23:01:41 +0300

    created user-service

---
+++ services/user-service/src/main/java/com/github/lotashinski/basketbuster/userservice/entity/UserReview.java
@@ -0,0 +1,35 @@
+package com.github.lotashinski.basketbuster.userservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "app_user_review")
+public class UserReview {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    
+    @Column(name = "text", nullable = false)
+    private String text;
+    
+    @Column(name = "starts", nullable = false)
+    private int starts;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+    
+}
